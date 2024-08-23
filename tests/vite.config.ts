ecbb20ea850c3309f32130cb99abb8c8b96e7cbc
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-08-23 12:05:33 +0200

    feat: Added test system for spec

---
+++ tests/vite.config.ts
@@ -0,0 +1,8 @@
+// vite.config.js / vite.config.ts
+import ViteYaml from '@modyfi/vite-plugin-yaml';
+
+export default {
+    plugins: [
+        ViteYaml(), // you may configure the plugin by passing in an object with the options listed below
+    ],
+};
